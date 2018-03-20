2011dd687bde878817fd7a2937afdb1a2067ae85
Daniel Oliveira <gbdaniel_rj@hotmail.com>
2018-03-20 00:12:50 -0300

    ajuste para atender a chamadas ajax

--- src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
+++ src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.algaworks.socialbooks.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,21 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("algaworks")
-		.password("s3nh4").roles("USER");
-	}
-	
-	protected void configure(HttpSecurity http) throws Exception {
-		http.
-			authorizeRequests()
-			.antMatchers("/h2-console/**").permitAll()
-			.anyRequest().authenticated()
-			.and()
-				.httpBasic()
-			.and()
-				.csrf().disable();
-	}
-	
+    @Autowired
+    public void configureGlobal( AuthenticationManagerBuilder auth ) throws Exception {
+        auth.inMemoryAuthentication().withUser( "algaworks" )
+                .password( "s3nh4" ).roles( "USER" );
+    }
+
+    @Override
+    protected void configure( HttpSecurity http ) throws Exception {
+        http.authorizeRequests()
+                .antMatchers( "/h2-console/**" ).permitAll()
+                .antMatchers( HttpMethod.OPTIONS, "/**" ).permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
+
 }
--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,68 +28,69 @@ import com.algaworks.socialbooks.services.LivrosService;
 @RequestMapping("/livros")
 public class LivrosResources {
 
-	@Autowired
-	private LivrosService livrosService;
+    @Autowired
+    private LivrosService livrosService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Livro>> listar() {
-		return ResponseEntity.status(HttpStatus.OK).body(livrosService.listar());
-	}
+    @CrossOrigin() // o certo é passar as origens como parâmetros
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Livro>> listar() {
+        return ResponseEntity.status( HttpStatus.OK ).body( livrosService.listar() );
+    }
 
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> salvar(@Valid @RequestBody Livro livro) {
-		livro = livrosService.salvar(livro);
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> salvar( @Valid @RequestBody Livro livro ) {
+        livro = livrosService.salvar( livro );
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
-				path("/{id}").buildAndExpand(livro.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path( "/{id}" ).buildAndExpand( livro.getId() )
+                .toUri();
 
-		return ResponseEntity.created(uri).build();
-	}
+        return ResponseEntity.created( uri ).build();
+    }
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
-		Livro livro = livrosService.buscar(id);
-		
-		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
-		
-		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livro);
-	}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> buscar( @PathVariable("id") Long id ) {
+        Livro livro = livrosService.buscar( id );
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
-		livrosService.deletar(id);
-		return ResponseEntity.noContent().build();
-	}
+        CacheControl cacheControl = CacheControl.maxAge( 20, TimeUnit.SECONDS );
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, 
-			@PathVariable("id") Long id) {
-		livro.setId(id);
-		livrosService.atualizar(livro);
-		
-		return ResponseEntity.noContent().build();
-	}
-	
-	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
-	public ResponseEntity<Void> adicionarComentario(@PathVariable("id") Long livroId, 
-			@RequestBody Comentario comentario) {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		comentario.setUsuario(auth.getName());
-		
-		livrosService.salvarComentario(livroId, comentario);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
-		
-		return ResponseEntity.created(uri).build();
-	}
-	
-	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.GET)
-	public ResponseEntity<List<Comentario>> listarComentarios(
-			@PathVariable("id")Long livroId) {
-		List<Comentario> comentarios = livrosService.listarComentarios(livroId);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(comentarios);
-	}
+        return ResponseEntity.status( HttpStatus.OK ).cacheControl( cacheControl ).body( livro );
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deletar( @PathVariable("id") Long id ) {
+        livrosService.deletar( id );
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> atualizar( @RequestBody Livro livro,
+            @PathVariable("id") Long id ) {
+        livro.setId( id );
+        livrosService.atualizar( livro );
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/comentarios", method = RequestMethod.POST)
+    public ResponseEntity<Void> adicionarComentario( @PathVariable("id") Long livroId,
+            @RequestBody Comentario comentario ) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        comentario.setUsuario( auth.getName() );
+
+        livrosService.salvarComentario( livroId, comentario );
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+
+        return ResponseEntity.created( uri ).build();
+    }
+
+    @RequestMapping(value = "/{id}/comentarios", method = RequestMethod.GET)
+    public ResponseEntity<List<Comentario>> listarComentarios(
+            @PathVariable("id") Long livroId ) {
+        List<Comentario> comentarios = livrosService.listarComentarios( livroId );
+
+        return ResponseEntity.status( HttpStatus.OK ).body( comentarios );
+    }
 }
